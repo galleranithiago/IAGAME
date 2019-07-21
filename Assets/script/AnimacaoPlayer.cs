a954e6275ccb0e486f6dcc6cce2d48bc643a7a51
galleranithiago <thiago_galelrani@yahoo.com.br>
2019-07-21 18:49:17 -0300

    commit inicial

---
+++ Assets/script/AnimacaoPlayer.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimacaoPlayer : MonoBehaviour {
+    public Animator Animador;
+    public float velocidade;
+    public float direcao;
+    public float soco;
+    public GameObject colisorSoco;
+    public float tempo;
+    public float dano;
+
+
+	// Use this for initialization
+	void Start () {
+        colisorSoco.SetActive(false);
+        tempo = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        velocidade = Input.GetAxis("Vertical");
+        direcao = Input.GetAxis("Horizontal");
+        soco = Input.GetAxis("Fire1");
+
+        Animador.SetFloat("velocidade", velocidade);
+        Animador.SetFloat("direcao", direcao);
+        Animador.SetFloat("soco", soco);
+
+        if (soco > 0.0f)
+        {
+            colisorSoco.SetActive(true);
+        }
+        else {
+            colisorSoco.SetActive(false);
+        }
+        tempo = tempo + (Time.deltaTime * 1);
+        if (tempo > 0.3) {
+            dano = 0;
+            Animador.SetFloat("damage", dano);
+        }
+    }
+    void OnTriggerEnter(Collider outro)
+    {
+        if (outro.gameObject.CompareTag("colisorSocoInimigo"))
+        {
+            tempo = 0;
+            dano = 1;
+            Animador.SetFloat("damage", dano);
+            
+            
+            
+            
+        }
+    }
+}
---
+++ Assets/script/Inimigo.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour {
+    public float campo;
+    public Transform player;
+    public float soco;
+    public GameObject inimigo;
+    public GameObject colisorSocoInimigo;
+    public float tempo;
+    public float dano;
+
+    public Animator Animador;
+    
+
+	// Use this for initialization
+	void Start () {
+        //colisorSocoInimigo.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+        campo = Vector3.Distance(player.position, transform.position);
+      
+        if (campo < 50) {
+            olhar();
+        }
+        if (campo < 30)
+        {
+            olhar();
+            seguir();
+        }
+        if (campo < 1.3f)
+        {
+            golpear();
+        }
+
+
+        tempo = tempo + (Time.deltaTime * 1);
+        if ((tempo > 2) && (dano > 0))
+        {
+            dano = 0;
+            inimigo.SetActive(false);
+        }
+        if ((tempo > 0.5f) && (soco > 0))
+        {
+            soco = 0;
+            Animador.SetFloat("soco", soco);
+        }
+           
+	}
+
+    void olhar() {
+        Quaternion visao;
+        visao = Quaternion.LookRotation(player.position -transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, visao, 6f * Time.deltaTime);           
+      }
+
+    void seguir()
+    {
+        //transform.Translate(Vector3.forward * 2 * Time.deltaTime);
+        Animador.SetFloat("velocidade", 1);
+     }
+    void golpear()
+    {
+        tempo = 0;
+        soco = 1;
+        Animador.SetFloat("soco", soco);
+        Animador.SetFloat("velocidade", 0);        
+    }
+
+    void OnTriggerEnter(Collider outro)
+    {
+        if (outro.gameObject.CompareTag("colisorSoco"))
+        {
+            tempo = 0;
+            dano = 1;
+            Animador.SetFloat("morte", dano);
+        }
+    }
+}
